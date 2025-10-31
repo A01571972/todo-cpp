928ea1276445506ce905c0a6c2785fa10107e73f
A01571972 <A01571972@tec.mx>
2025-10-31 17:21:00 -0600

    Agregue los metdodos de la clase Task

--- Task.h
+++ Task.h
@@ -1,9 +1,30 @@
 #ifndef TASK_H
 #define TASK_H
 
+#include <string>
+
 class Task {
     public:
+    // constructor
+    Task(std::string title, bool done, std::string description);
+
+    // getters
+    std::string getTitle();
+    bool getDone();
+    std::string getDescription();
+
+    // setters
+    void setTitle(std::string title);
+    void setDone(bool done);
+    void setDescription(std::string description);
+
+    // methods
+    void markDone();
+    
     private:
+    std::string title;
+    bool done;
+    std::string description;
 };
 
 
