f216884315a59ed21b6b92fe615d67822510bd53
A01571972 <A01571972@tec.mx>
2025-10-31 17:25:16 -0600

    Implementación de los metodos

--- Task.h
+++ Task.h
@@ -20,12 +20,46 @@ class Task {
 
     // methods
     void markDone();
-    
+
     private:
     std::string title;
     bool done;
     std::string description;
 };
+
+// constructor
+Task::Task(std::string title, bool done, std::string description) {
+    this->title = title;
+    this->done = done;
+    this->description = description;
+}
+
+// getters
+std::string Task::getTitle() {
+    return this->title;
+}
+bool Task::getDone() {
+    return this->done;
+}
+std::string Task::getDescription() {
+    return this->description;
+}
+
+// setters
+void Task::setTitle(std::string title) {
+    this->title = title;
+}
+void Task::setDone(bool done) {
+    this->done = done;
+}
+void Task::setDescription(std::string description) {
+    this->description = description;
+}
+
+// methods
+void Task::markDone() {
+    this->done = true;
+}
 
 
 #endif // TASK_H
